5805e13693f5672512a5fbff3de3a1e8de7663a7
Daniel Hails <danielh.c1@outlook.com>
2026-09-17 16:31:21 +0100

    Added mini boss scene for the end of level 2; instantiated it

--- hero-of-greece/Scripts/enemySpawner.gd
+++ hero-of-greece/Scripts/enemySpawner.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var enemyScene = preload("res://Scenes/enemy1.tscn")
+const MINI_BOSS = preload("res://Scenes/miniBoss.tscn")
 
 func _ready() -> void:
 	var enemySpawns
@@ -8,6 +9,7 @@ func _ready() -> void:
 		enemySpawns = [Vector2(100, -75), Vector2(475, -70), Vector2(850, -50)]
 	elif Globals.level == 2:
 		enemySpawns = [Vector2(100, -75), Vector2(475, -70), Vector2(1200, -100)]
+		spawnMiniBoss(Vector2(1900, -100))
 	for i in range(len(enemySpawns)):
 		spawnEnemy(enemySpawns[i])
 		
@@ -16,4 +18,12 @@ func spawnEnemy(spawn: Vector2) -> void:
 	enemy.add_to_group("enemies")
 	enemy.position = spawn
 	add_child(enemy)
+	
+func spawnMiniBoss(spawn: Vector2) -> void:
+	var boss = MINI_BOSS.instantiate()
+	boss.add_to_group("enemies")
+	boss.position = spawn
+	add_child(boss)
+	
+
 	
